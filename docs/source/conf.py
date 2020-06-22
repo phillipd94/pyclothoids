535256df723b42ccdf2aa509ea69c9fe479956b7
phillipd94 <phildix11@gmail.com>
2020-06-22 15:41:04 -0500

    added master-doc option to configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ author = 'Phillip Dix'
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
